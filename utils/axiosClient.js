d848926000813c5108c8e04d8d6f0ca767e9f6e3
VinayRana54640 <vinayrana54640@gmail.com>
2025-10-02 17:52:02 +0530

    change relative path

--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://job4grads.com/", //process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/",
+  baseURL: "/", //process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/",
 });
 
 // Add request interceptor
