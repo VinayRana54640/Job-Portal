d43687c95073590e8dc4a5eb419e6aeb4fb60b5b
VinayRana54640 <vinayrana54640@gmail.com>
2025-10-07 21:31:28 +0530

    sitemaps uploaded

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://www.yourdomain.com",
+  siteUrl: process.env.SITE_URL || "https://www.job4grads.com",
   generateRobotsTxt: true, // generate robots.txt
   sitemapSize: 5000, // optional, splits large sitemaps
   changefreq: "daily",
